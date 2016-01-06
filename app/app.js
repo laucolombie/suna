c7346c92c6736ffe8165e0da11a6b8ba6f6a1dde
Laurence Colombie <laurencecolombie@Laurences-MBP.home>
2016-01-06 20:06:57 +0000

    index page first test

--- app/app.js
+++ app/app.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import IndexPage from './IndexPage.js'
+
 ReactDOM.render(
-  <div>Hello!</div>,
+  <IndexPage/>,
   document.getElementById('app-container')
 );
+
